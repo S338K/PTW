e7e8dfd21415d5e265b952f927a9f2958583f17a
S338K <S338K@outlook.com>
2025-09-15 14:28:16 +0300

    Updated

--- backend/server.js
+++ backend/server.js
@@ -38,9 +38,8 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+    secure: true,
+    sameSite: 'none',
     maxAge: 1000 * 60 * 60 * 24
   }
 }));
