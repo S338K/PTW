cd3291394d3b739c36a92fb421fb46660df21f71
S338K <S338K@outlook.com>
2025-09-15 09:26:51 +0300

    added log

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+console.log('MONGO_URI from env:', process.env.MONGO_URI);
 const express = require('express');
 const mongoose = require('mongoose');
 const session = require('express-session');
