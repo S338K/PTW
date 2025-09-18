1313fb11162c653b41f11d0f56bad18f4350c974
S338K <S338K@outlook.com>
2025-09-19 01:27:31 +0300

    update

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const isProd = process.env.NODE_ENV === 'production';
 
 // ================= MIDDLEWARE =================
 // Body parsers
-app.use(express.json());
+//app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // ===== CORS Setup =====
