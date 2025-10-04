0574607baf71ea2f4af1f31c00a53e676e1e76ef
S338K <S338K@outlook.com>
2025-10-04 09:09:51 +0300

    up

--- backend/server.js
+++ backend/server.js
@@ -12,10 +12,6 @@ const apiRoutes = require('./routes/api');
 const puppeteer = require('puppeteer-core');
 const chromium = require('@sparticuz/chromium');
 const path = require("path");
-const User = require("../models/User");
-const Admin = require("../models/Admin");
-const Approver = require("../models/Approver");
-const Approver = require("../models/preApprover");
 
 const isProd = process.env.NODE_ENV === 'production';
 const app = express();
